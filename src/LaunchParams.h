62aebd6bf9ac2a8efd95cc7e1995fe678b7c481f
Julio <juliomorerofing@gmail.com>
2020-10-11 22:14:23 -0300

    Changed base to ex10

--- src/LaunchParams.h
+++ src/LaunchParams.h
@@ -20,39 +20,43 @@
 #include "optix7.h"
 
 namespace osc {
-	using namespace gdt;
+  using namespace gdt;
 
-	struct TriangleMeshSBTData {
-		vec3f  color;
-		vec3f* vertex;
-		vec3i* index;
-	};
+  // for this simple example, we have a single ray type
+  enum { RADIANCE_RAY_TYPE=0, SHADOW_RAY_TYPE, RAY_TYPE_COUNT };
 
-	struct Light2 {
-		vec3f position;
-		vec3f power;
-		int numberPhotons;
-	};
+  struct TriangleMeshSBTData {
+    vec3f  color;
+    vec3f *vertex;
+    vec3f *normal;
+    vec2f *texcoord;
+    vec3i *index;
+    bool                hasTexture;
+    cudaTextureObject_t texture;
+  };
+  
+  struct LaunchParams
+  {
+    struct {
+      uint32_t *colorBuffer;
+      vec2i     size;
+      int       accumID { 0 };
+    } frame;
+    
+    struct {
+      vec3f position;
+      vec3f direction;
+      vec3f horizontal;
+      vec3f vertical;
+    } camera;
 
-	struct LaunchParams
-	{
-		struct {
-			uint32_t* colorBuffer;
-			vec2i     size;
-		} frame;
+    struct {
+      vec3f origin, du, dv, power;
+    } light;
 
-		struct {
-			vec3f position;
-			vec3f direction;
-			vec3f horizontal;
-			vec3f vertical;
-		} camera;
-
-		struct {
-			Light2* lights;
-		};
-
-		OptixTraversableHandle traversable;
-	};
+    double* halton;
+    
+    OptixTraversableHandle traversable;
+  };
 
 } // ::osc
